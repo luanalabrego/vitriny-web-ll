95d064b31573142de1a726a586a94631473221e8
Luana <luanasomogyi@gmail.com>
2025-05-08 17:21:47 -0300

    Descrição das alterações

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzx3Dfm0bTTJuf3hSaJjb0F2KK4kmEjIg",
-  authDomain: "vitrinyweb11.firebaseapp.com",
-  projectId: "vitrinyweb11",
-  storageBucket: "vitrinyweb11.appspot.com", // <- aqui corrigido
-  messagingSenderId: "1069607586521",
-  appId: "1:1069607586521:web:5a77642bbb31faf15d8cc8"
+  apiKey: "AIzaSyBoVVRf6UMK-e6uGUkmNvnnp7HZz0iSClQ",
+  authDomain: "vitriny-web.firebaseapp.com",
+  projectId: "vitriny-web",
+  storageBucket: "vitriny-web.firebasestorage.app", // <- aqui corrigido
+  messagingSenderId: "343502550145",
+  appId: "1:343502550145:web:a3ccccbc8fd5a5bee14980"
 };
 
 const app = initializeApp(firebaseConfig);
